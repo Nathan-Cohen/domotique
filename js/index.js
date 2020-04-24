392ebdb6e5029e005a6a20a15f7ed98fc5ebc694
nathan cohen <manathane.co@gmail.com>
2020-04-24 17:52:59 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+function matin(){
+    $("#matin").button('toggle')
+}
+
+function soiree(){
+    $("#soiree").button('toggle')
+}
+
+function nuit(){
+    $("#nuit").button('toggle')
+}
+
+function reveil(){
+    $("#reveil").button('toggle')
+
+}
